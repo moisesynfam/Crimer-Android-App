85bf8aaa51b3ccfa2b11b2bf442a1e7494ac9a81
Moises Ynfante <moises.ynfante@pushmotion.com>
2018-06-13 21:40:48 -0400

    Updated form method to save to database

--- app/src/main/java/com/ynfante/crimer/RegisterActivity.java
+++ app/src/main/java/com/ynfante/crimer/RegisterActivity.java
@@ -21,14 +21,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
@@ -37,6 +44,7 @@ import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.mobsandgeeks.saripaar.annotation.Password;
 import com.myhexaville.smartimagepicker.ImagePicker;
 import com.myhexaville.smartimagepicker.OnImagePickedListener;
+import com.ynfante.crimer.Models.User;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -58,6 +66,9 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
     private EditText name;
 
     @NotEmpty
+    private EditText username;
+
+    @NotEmpty
     @Email
     private EditText email;
 
@@ -77,7 +88,12 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
     private ImagePickerBottomSheetFragment imagePickerFragment;
 
     private ImagePicker imagePicker;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference profilePictureReference;
+    private FirebaseFirestore database;
 
+
+    private MaterialDialog userProgressDialog, imageErrorDialog, userErrorDialog;
 
 
 
@@ -86,6 +102,7 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        username = findViewById(R.id.register_username_input);
         name = findViewById(R.id.register_name_input);
         password = findViewById(R.id.register_password_input);
         email = findViewById(R.id.register_email_input);
@@ -150,12 +167,36 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
 
             }
         });
-
+        mAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        profilePictureReference = firebaseStorage.getReference().child("images/profilePictures");
+        database = FirebaseFirestore.getInstance();
+        buildDialogs();
 
 
 
     }
 
+    public void  buildDialogs() {
+        userProgressDialog = new MaterialDialog.Builder(this)
+                .title(R.string.register_user_loading_title)
+                .progress(true, -1)
+                .autoDismiss(false)
+                .cancelable(false)
+                .build();
+
+        imageErrorDialog = new MaterialDialog.Builder(this)
+                .title(R.string.register_image_uploading_error_title)
+                .content(R.string.register_image_uploading_error_content)
+                .positiveText(R.string.general_dismiss)
+                .build();
+
+        userErrorDialog = new MaterialDialog.Builder(this)
+                .title(R.string.register_user_error_title)
+                .content(R.string.register_user_error_content)
+                .positiveText(R.string.general_dismiss)
+                .build();
+    }
 
     public void chooseImage() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
@@ -175,6 +216,7 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         imagePicker.handleActivityResult(resultCode, requestCode, data);
+
         if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
                 && data != null && data.getData() != null )
         {
@@ -218,6 +260,7 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
     @Override
     public void onValidationSucceeded() {
         Log.d(TAG, "Form succeded");
+        registerUser(name.getText().toString(), username.getText().toString(), email.getText().toString(), password.getText().toString());
     }
 
     @Override
@@ -235,8 +278,9 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
         }
     }
 
-    public void registerUser(String name, String email, String password) {
-
+    public void registerUser(final String name, final String username, String email, String password) {
+        userProgressDialog.show();
+        Log.d(TAG, "registerUser:started");
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -245,13 +289,75 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-
+                            storeNewUser(user, name, username);
 
                         } else {
-
+                            Log.d(TAG, "createUserWithEmail:fail");
+                            Log.d(TAG, task.getException().getMessage());
                         }
                     }
                 });
+    }
+
+    public void storeNewUser(final FirebaseUser user, String name, String username) {
+        Log.d(TAG, "storeNewUser:savingToDatabase");
+        User newUser = new User(username, name, null, null);
+        database.collection("users").document(user.getUid()).set(newUser)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    storeProfilePicture(user);
+                    Log.d(TAG, "USER SAVED");
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(TAG, e.getMessage());
+                }
+            });
+    }
+
+    public void storeProfilePicture (final FirebaseUser user) {
+        if( profilePicturePath != null) {
+            final StorageReference newPictureRef = profilePictureReference.child(user.getUid() + ".jpeg");
+            UploadTask profileUploadTask = newPictureRef.putFile(profilePicturePath);
+            profileUploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = newPictureRef.getDownloadUrl().getResult();
+                        saveProfilePicture(user, downloadUri);
+                    } else {
+                        //here we display error message
+                        goToPosts();
+                    }
+                }
+            });
+        } else {
+            goToPosts();
+        }
+    }
+
+    public void goToPosts() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void saveProfilePicture(FirebaseUser user, Uri downloadUrl) {
+
+        database.collection("users").document(user.getUid()).update("photoUrl", downloadUrl)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if( !task.isSuccessful() ) {
+                            Log.w(TAG, task.getException().getMessage());
+                        }
+                        goToPosts();
+                    }
+                });
+
     }
 
     private boolean checkForPicturesPermissions(boolean forCamera) {
